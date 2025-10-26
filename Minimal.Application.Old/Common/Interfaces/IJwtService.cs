9f4c5bdd4efeae731c282f8382d5dfb6b287a75e
Luan <luanveloz@gmail.com>
2025-10-26 16:12:43 +0100

    refactor: remove all unnecessary files

--- Minimal.Application.Old/Common/Interfaces/IJwtService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Minimal.Application.Common.Results;
-
-namespace Minimal.Application.Common.Interfaces;
-
-public interface IJwtService
-{
-    ServiceResult<string> GenerateToken(Guid userId, string email, ICollection<string> roles);
-}
--- Minimal.Application.Old/Common/Interfaces/IVerbImporterService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Minimal.Application.Common.DTOs;
-using Minimal.Application.Common.Results;
-
-namespace Minimal.Application.Common.Interfaces;
-
-public interface IVerbImporterService
-{
-    Task<ServiceResult<VerbConjugationData?>> GetVerbConjugation(string verb, CancellationToken ct);
-}
